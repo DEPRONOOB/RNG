ca303360b9507d401c8fe4e6266aa77a808a2b9f
DEPRONOOB <70565173+DEPRONOOB@users.noreply.github.com>
2024-02-22 22:55:26 +1300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const myButton = document.querySelector(".nav-btn");
-    const box = document.getElementById("sidebar");
-  
-    myButton.addEventListener("click", function() {
-      box.classList.toggle('show');
-    });
-  });
